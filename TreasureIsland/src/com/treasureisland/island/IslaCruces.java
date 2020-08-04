95222ac0010f529bf28b8b7e18dd738c825b8457
josh <shike25@gmail.com>
2020-08-04 16:17:51 -0400

    adding getter and setter

--- TreasureIsland/src/com/treasureisland/island/IslaCruces.java
+++ TreasureIsland/src/com/treasureisland/island/IslaCruces.java
@@ -1,4 +1,14 @@
 package com.treasureisland.island;
 
 public class IslaCruces extends Island {
+
+  @Override
+  public void setIslandToTheNorth(Island islandToTheNorth) {
+    super.setIslandToTheNorth(islandToTheNorth);
+  }
+
+  @Override
+  public void setIslandToTheWest(Island islandToTheWest) {
+    super.setIslandToTheWest(islandToTheWest);
+  }
 }
--- TreasureIsland/src/com/treasureisland/island/IslaDeMuerta.java
+++ TreasureIsland/src/com/treasureisland/island/IslaDeMuerta.java
@@ -1,4 +1,14 @@
 package com.treasureisland.island;
 
 public class IslaDeMuerta extends Island {
+
+  @Override
+  public void setIslandToTheSouth(Island islandToTheSouth) {
+    super.setIslandToTheSouth(islandToTheSouth);
+  }
+
+  @Override
+  public void setIslandToTheEast(Island islandToTheEast) {
+    super.setIslandToTheEast(islandToTheEast);
+  }
 }
