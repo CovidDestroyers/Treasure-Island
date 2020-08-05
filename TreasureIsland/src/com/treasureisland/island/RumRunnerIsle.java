bf018fbb9272996ff52ef21bef9eca44b95c819e
josh <shike25@gmail.com>
2020-08-04 20:26:25 -0400

    basis for pointing to scenes and islands

--- TreasureIsland/src/com/treasureisland/island/RumRunnerIsle.java
+++ TreasureIsland/src/com/treasureisland/island/RumRunnerIsle.java
@@ -3,53 +3,12 @@ package com.treasureisland.island;
 import com.treasureisland.world.*;
 
 public class RumRunnerIsle extends Island {
-  Scene location;
 
 
-  public void setIslandToTheNorth(Island islandToTheNorth) {
-    super.setIslandToTheNorth(islandToTheNorth);
+  public RumRunnerIsle() {
+    setNorthScene(new RumDistillery());
+    setEastScene(new AbandonedDistillery());
+    setSouthScene(new CrimsonBeachBar());
+    setWestScene(new SugarCaneField());
   }
-
-
-  public void setIslandToTheEast(Island islandToTheEast) {
-    super.setIslandToTheEast(islandToTheEast);
-  }
-
-
-  public void setNorthScene(RumDistillery northScene) {
-    super.setNorthScene(northScene);
-  }
-
-
-  public void setSouthScene(CrimsonBeachBar southScene) {
-    super.setSouthScene(southScene);
-  }
-
-
-  public void setWestScene(SugarCaneField westScene) {
-    super.setWestScene(westScene);
-  }
-
-
-  public void setEastScene(AbandonedDistillery eastScene) {
-    super.setEastScene(eastScene);
-  }
-
-  // @Override
-  // public Location islandLocationSelector(IslandSelector rumRunnerIsle, DirectionEnum direction) {
-  //   location = null;
-  //   if (direction.equals(DirectionEnum.N)) {
-  //     location = new RumDistillery();
-  //   }
-  //   if (direction.equals(DirectionEnum.S)) {
-  //     location = new CrimsonBeachBar();
-  //   }
-  //   if (direction.equals(DirectionEnum.E)) {
-  //     location = new AbandonedDistillery();
-  //   }
-  //   if (direction.equals(DirectionEnum.W)) {
-  //     location = new SugarCaneField();
-  //   }
-  //   return location;
-  // }
 }
