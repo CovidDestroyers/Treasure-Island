582ec3875fc9181ded0a476ef9ae0fba6abe5931
David Hull <david.hull37@protonmail.com>
2020-08-07 15:41:51 -0500

    Added user input validation using DirectionEnum

--- TreasureIsland/src/com/treasureisland/island/DirectionEnum.java
+++ TreasureIsland/src/com/treasureisland/island/DirectionEnum.java
@@ -1,5 +1,42 @@
 package com.treasureisland.island;
 
 public enum DirectionEnum {
-    NORTH, SOUTH, EAST, WEST
+  N("N"),
+  S("S"),
+  E("E"),
+  W("W"),
+  NORTH("NORTH"),
+  SOUTH("SOUTH"),
+  EAST("EAST"),
+  WEST("WEST");
+
+  private String acceptedText;
+
+  DirectionEnum(String acceptedText) {
+    this.acceptedText = acceptedText;
+  }
+
+  public String getAcceptedText() {
+    return this.acceptedText;
+  }
+
+  public static Boolean isValid(String userInput) {
+    boolean isValid = false;
+    if (fromString(userInput) != null) {
+      isValid = true;
+    }
+    return isValid;
+  }
+
+  public static DirectionEnum fromString(String acceptedText) {
+    DirectionEnum foundDirection = null;
+
+    for (DirectionEnum direction : DirectionEnum.values()) {
+      if (direction.acceptedText.equalsIgnoreCase(acceptedText)) {
+        foundDirection = direction;
+        break;
+      }
+    }
+    return foundDirection;
+  }
 }
