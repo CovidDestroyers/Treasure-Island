d27a0cb7b12ca5fe0e57c568fa7b08798e89259a
David Hull <david.hull37@protonmail.com>
2020-08-08 17:25:55 -0500

    Added Vendor interaction prompt

--- TreasureIsland/src/com/treasureisland/scene/SugarCaneField.java
+++ TreasureIsland/src/com/treasureisland/scene/SugarCaneField.java
@@ -3,7 +3,6 @@ package com.treasureisland.scene;
 import com.treasureisland.items.Item;
 import com.treasureisland.player.Color;
 import com.treasureisland.player.Player;
-import java.util.Scanner;
 
 public class SugarCaneField extends Scene {
 
@@ -11,62 +10,67 @@ public class SugarCaneField extends Scene {
     super(sceneName);
   }
 
-
   @Override
   public void talkToNPC(Player player) {
     System.out.println("I won't talk without the secret code");
     String input = scanner.nextLine();
     if (input.equalsIgnoreCase("9999")) {
-      System.out.println("\n "
-          + Color.ANSI_BOLD.getValue()
-          + Color.ANSI_BLUE.getValue()
-          + "DING DING DING"
-          + Color.ANSI_RESET.getValue()
-          + "\n You have unlocked your first treasure piece."
-          + "\n awarded "
-          + Color.ANSI_BOLD.getValue()
-          + Color.ANSI_RED.getValue()
-          + "\"An Emerald\""
-          + Color.ANSI_RESET.getValue());
+      System.out.println(
+          "\n "
+              + Color.ANSI_BOLD.getValue()
+              + Color.ANSI_BLUE.getValue()
+              + "DING DING DING"
+              + Color.ANSI_RESET.getValue()
+              + "\n You have unlocked your first treasure piece."
+              + "\n awarded "
+              + Color.ANSI_BOLD.getValue()
+              + Color.ANSI_RED.getValue()
+              + "\"An Emerald\""
+              + Color.ANSI_RESET.getValue());
       player.playerTreasures.add("Emerald");
       player.playerInventory.remove(Item.findByName(player.playerInventory, "9"));
       player.playerInventory.remove(Item.findByName(player.playerInventory, "999"));
       player.setHasIslandItem(true);
     } else {
-      System.out.println(
-        "To get the code, you should look around for the clues.");
+      System.out.println("To get the code, you should look around for the clues.");
     }
   }
 
   @Override
   public void lookAroundLocation(Player player) {
-    storylineProgression("TI.txt",  "WWStart", "WWEnd");
+    storylineProgression("TI.txt", "WWStart", "WWEnd");
     storylineProgression("TI.txt", "SStart", "SEnd");
     player.setPlayerHealth(player.getPlayerHealth() - 5);
     System.out.println("\n");
     player.playerCoinGenerator();
     System.out.println("\n");
     player.iterateThroughClues();
-    if(Item.findByName(player.playerInventory, "Key") == null) {
+    if (Item.findByName(player.playerInventory, "Key") == null) {
       player.playerInventory.add(new Item("Key", 0, 0));
-      System.out.println("\n" + Color.ANSI_BLUE.getValue() + "Clue added to your Inventory!!" + Color.ANSI_RESET.getValue());
+      System.out.println(
+          "\n"
+              + Color.ANSI_BLUE.getValue()
+              + "Clue added to your Inventory!!"
+              + Color.ANSI_RESET.getValue());
     }
-
-    // TODO decide what we want to do for the key. Will we need an object ArrayList to hold items?
-    //player.playerClues.add("key");
   }
-
-//  @Override
-//  public void investigateArea(Player player) {
-//    storylineProgression("TI.txt", "SStart", "SEnd");
-//    player.setPlayerHealth(player.getPlayerHealth() - 5);
-//
-//    // TODO decide what we want to do for the key. Will we need an object ArrayList to hold items?
-//    player.playerClues.add("some placeholder for the key");
-//  }
 
   @Override
   public void vendor(Player player) {
     player.playerVisitsVendor();
+  }
+
+  @Override
+  public void displayInteractionOptions() {
+    System.out.println(
+        "\nWhat would you like to do?\n "
+            + " -Type \"T\": Talk\n "
+            + " -Type \"L\": Look Around\n "
+            + " -Type \"V\": Visit Vendor\n "
+            + " -Type \"R\": See Treasure Rewards\n "
+            + " -Type \"M\": Look at the Map\n "
+            + " -Type \"INV\": Inventory\n "
+            + " -Type \"G\": Grab Item\n "
+            + " -Type \"E\": Exit This Location\n ");
   }
 }
